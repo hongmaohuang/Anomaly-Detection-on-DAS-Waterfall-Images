e9acd724ac33efcba5d426489c60b83cfe16d591
hongmaohuang <al47777al4@gmail.com>
2025-04-09 18:06:33 +0800

    New Feature: Save all waveforms

--- 03_Extract_Anamoly_Waveform.py
+++ 03_Extract_Anamoly_Waveform.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 import obspy
 import os
 from pathlib import Path
-
+from tqdm import tqdm
 
 # Open and read header
 with open('anomaly_points.log', 'r') as f:
@@ -28,14 +28,29 @@ data_dir = Path('./DAS_data/20250331/waveforms')
 waveform_files = glob.glob(str(data_dir / '*.hdf5'))
 
 # Process DAS data
-ds_all = [xdas.open_mfdataarray(f, engine="asn") for f in waveform_files]
+ds_all = [xdas.open_mfdataarray(f, engine="asn") for f in tqdm(waveform_files, desc='讀取進度')]
 ds_all_sorted = sorted(ds_all, key=lambda ds: ds.coords['time'].values[0])
 ds_all_xr_sorted = [xr.DataArray(ds.values, 
                     coords={'time': ds.coords['time'], 'distance': ds.coords['distance']},
                     dims=['time', 'distance']) 
                     for ds in ds_all_sorted]
 ds_concat_xr = xr.concat(ds_all_xr_sorted, dim='time')
+ds_concat_xr.to_netcdf('ds_concat_xr_20250331.nc')
 
+''' 
+time = ds_concat_xr.time.values
+distance = ds_concat_xr.distance.values
+data = ds_concat_xr.values
+plt.figure(figsize=(12, 6))
+plt.pcolormesh(distance, time, data, shading='auto', cmap='seismic', vmax=200, vmin=-200)
+plt.colorbar(label='Strain or Amplitude')
+plt.xlabel('Distance (km)')
+plt.ylabel('Time')
+plt.gca().invert_yaxis()
+'''
+
+
+# %%
 # Create output directory
 output_dir = Path('./Detected_anomalies')
 output_dir.mkdir(exist_ok=True)
