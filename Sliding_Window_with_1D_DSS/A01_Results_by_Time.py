1e36533bcc350c07e08fcf9887d2334ebc4508cf
hongmaohuang <al47777al4@gmail.com>
2025-04-25 11:53:26 +0800

    New code for your testing on long period dataset

---
+++ Sliding_Window_with_1D_DSS/A01_Results_by_Time.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+import subprocess
+import glob
+import config
+
+# Please make sure you also update the parameters in config.py !
+all_files = glob.glob('/home/hmhuang/Work/Hualien_DAS_Monitoring/waterfall_images_17min_0423/*.png')
+
+def extract_datetime(file_path):
+    basename = os.path.basename(file_path)
+    parts = basename.split('_')  # i.e. ['Waterfall', 'RMS', '20250331', '020551', 'utc.png']
+    date_part = parts[2]         # i.e. '20250331'
+    time_part = parts[3]         # i.e. '020551'
+    return int(date_part + time_part)  # i.e. 20250331020551（int）
+
+files_sorted = sorted(all_files, key=extract_datetime)
+
+for file_path in files_sorted:
+    print(f"\nProcessing: {file_path}")
+
+    # Make sure only one file is in the input folder
+    if os.path.exists(config.DAS_WATERFALL_PATH):
+        shutil.rmtree(config.DAS_WATERFALL_PATH)
+    os.makedirs(config.DAS_WATERFALL_PATH)
+
+    # Copy files to input folder
+    shutil.copy(file_path, config.DAS_WATERFALL_PATH)
+
+    # script
+    scripts_to_run = [
+        '01_Image_Clips.py',
+        '02_Feature_Functions.py',
+        '03_Scattering_Network_Design.py',
+        '04_Scattering_Transform.py',
+        '05_PCA.py',
+        '06_Clustering.py'
+    ]
+
+    for script in scripts_to_run:
+        print(f"Running {script}")
+        result = subprocess.run(['python', script], capture_output=True, text=True)
+        if result.returncode != 0:
+            print(f"Error running {script}: {result.stderr}")
+            break  
+
+print("\nAll files processed.")
