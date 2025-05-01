0715f3afbaf9573980c7e97c06d22a2f7925e93c
hongmaohuang <al47777al4@gmail.com>
2025-05-01 10:01:20 +0800

    The setting right in this version is nice for new datasets after 250430

--- Sliding_Window_with_1D_DSS/config.py
+++ Sliding_Window_with_1D_DSS/config.py
@@ -17,9 +17,11 @@ WAVELET_FOLDER = os.path.join(OUTPUT_PATH, "wavelets")
 SCATTERING_COEFFICIENTS_FOLDER = os.path.join(OUTPUT_PATH, "scattering_coefficients")
 PCA_ICA_FOLDER = os.path.join(OUTPUT_PATH, "pca_ica")
 CLUSTERING_RESULTS_FOLDER = os.path.join(OUTPUT_PATH, "clustering_results")
+CLUSTERING_RESULTS_FOLDER_CLUSTERS = os.path.join(CLUSTERING_RESULTS_FOLDER, "clusters")
+CLUSTERING_RESULTS_FOLDER_CLUSTERS_DIST = os.path.join(CLUSTERING_RESULTS_FOLDER, "clusters_dist_log")
 
 # ==== Image Crops ==== #
-CROP_OR_NOT = 'NO'  # 'YES' or 'NO'
+CROP_OR_NOT = 'YES'  # 'YES' or 'NO'
 CROP_TOP = 160
 CROP_BOTTOM = 696
 CROP_LEFT = 70
@@ -37,12 +39,12 @@ TOTAL_DISTANCE_KM = max_distance / 1000
 TOTAL_DURATION_SEC = DURATION_WATERFALL * 60
 
 # ==== Sliding Window Parameters ==== #
-WINDOW_SIZE_CHANNEL = 5
+WINDOW_SIZE_CHANNEL = 3
 STEP_CHANNEL = 2
 
 # ==== Scattering Network Parameters ==== #
-SEGMENT_DISTANCE = 0.04  # in km
-SEGMENT_OVERLAP = 0.2  # as fraction (e.g., 0.2 = 20% overlap)
+SEGMENT_DISTANCE = 0.02  # in km
+SEGMENT_OVERLAP = 0.5  # as fraction (e.g., 0.2 = 20% overlap)
 OCTAVES_1 = 6
 RESOLUTION_1 = 10
 QUALITY_1 = 1
@@ -51,13 +53,13 @@ RESOLUTION_2 = 8
 QUALITY_2 = 1
 
 # ==== PCA and ICA ==== #
-PCA_COMPONENTS = 2
+PCA_COMPONENTS = 20
 ICA_COMPONENTS = 3 # Could be estimated using PCA Result 
 
 # ==== Clustering Parameters ==== #
-CLUSTER_METHOD = "agglomerative"   # "kmeans", "gmm", "dbscan", "agglomerative"
+CLUSTER_METHOD = "gmm"   # "kmeans", "gmm", "dbscan", "agglomerative"
 KMEANS_CLUSTERS = 5
-GMM_N_COMPONENTS = 4
+GMM_N_COMPONENTS = 5
 GMM_COVARIANCE_TYPE = "full"
 DBSCAN_EPS = 0.6
 DBSCAN_MIN_SAMPLES = 50
@@ -65,7 +67,7 @@ AGG_N_CLUSTERS = 5
 AGG_LINKAGE = "single"  # "ward", "complete", "average", "single"
 
 # ==== Visualization Parameters ==== #
-SMOOTH_KERNEL = 20
+SMOOTH_KERNEL = 5
 
 
 
