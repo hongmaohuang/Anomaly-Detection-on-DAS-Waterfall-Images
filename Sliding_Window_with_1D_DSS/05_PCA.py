b4e80aef69df23342188e86e04fcb32a1fd2957b
hongmaohuang <al47777al4@gmail.com>
2025-05-01 10:35:11 +0800

    Just Run ICA if PCA parameters are fixed. Users can define them in config.py

--- Sliding_Window_with_1D_DSS/05_PCA.py
+++ Sliding_Window_with_1D_DSS/05_PCA.py
@@ -28,7 +28,6 @@ scattering = np.where(scattering_coefficients <= 0, 1e-10, scattering_coefficien
 # transform into log
 scattering_coefficients = np.log10(scattering)
 
-
 # 5. Normalize
 scattering = StandardScaler().fit_transform(scattering_coefficients)
 
@@ -36,38 +35,43 @@ scattering = StandardScaler().fit_transform(scattering_coefficients)
 n_distance, n_coeff = scattering_coefficients.shape
 
 # Apply PCA
-pca_model = PCA(n_components=config.PCA_COMPONENTS, whiten=True)
-pca_features = pca_model.fit_transform(scattering_coefficients)
+if config.RUN_PCA == "NO":
+    print("You are not running PCA")
 
-# Plot the result from PCA
-# Normalize features for display
-features_normalized = pca_features / np.abs(pca_features).max(axis=0)
+if config.RUN_PCA == "YES":
+    print("You are running PCA, please check the turning points in fig. and use it for ICA")
+    pca_model = PCA(n_components=config.PCA_COMPONENTS, whiten=True)
+    pca_features = pca_model.fit_transform(scattering_coefficients)
 
-# Figure and axes instance
-fig, axes = plt.subplots(1,3,figsize=(15,5),dpi=300)
-axes[0].plot(np.cumsum(pca_model.explained_variance_ratio_/sum(pca_model.explained_variance_ratio_)),'.-')
-axes[0].set_ylabel("cumsum variance")
-axes[0].set_xlabel("Feature index")
-axes[0].set_title("Information content\nof each feature")
+    # Plot the result from PCA
+    # Normalize features for display
+    features_normalized = pca_features / np.abs(pca_features).max(axis=0)
 
-# Plot features in distance
-axes[1].plot(distance, features_normalized + np.arange(features_normalized.shape[1]), rasterized=True)
-axes[1].set_xlim(distance.min(),distance.max())
-axes[1].set_ylabel("distance index")
-axes[1].set_xlabel("Distance")
-axes[1].set_title("Features in Distance")
+    # Figure and axes instance
+    fig, axes = plt.subplots(1,3,figsize=(15,5),dpi=300)
+    axes[0].plot(np.cumsum(pca_model.explained_variance_ratio_/sum(pca_model.explained_variance_ratio_)),'.-')
+    axes[0].set_ylabel("cumsum variance")
+    axes[0].set_xlabel("Feature index")
+    axes[0].set_title("Information content\nof each feature")
 
-# Plot the first two principal components
-mappable = axes[2].scatter(pca_features[:,0],pca_features[:,1],s=1,c=distance)
-axes[2].set_ylabel("Feature 1")
-axes[2].set_xlabel("Feature 0")
-axes[2].set_title("First two principal components")
+    # Plot features in distance
+    axes[1].plot(distance, features_normalized + np.arange(features_normalized.shape[1]), rasterized=True)
+    axes[1].set_xlim(distance.min(),distance.max())
+    axes[1].set_ylabel("distance index")
+    axes[1].set_xlabel("Distance")
+    axes[1].set_title("Features in Distance")
 
-cbar_ax = plt.colorbar(mappable)
-cbar_ax.ax.set_ylim(distance.min(),distance.max())
+    # Plot the first two principal components
+    mappable = axes[2].scatter(pca_features[:,0],pca_features[:,1],s=1,c=distance)
+    axes[2].set_ylabel("Feature 1")
+    axes[2].set_xlabel("Feature 0")
+    axes[2].set_title("First two principal components")
 
-# Show
-plt.savefig(f"{config.PCA_ICA_FOLDER}/pca.png", dpi=300)
+    cbar_ax = plt.colorbar(mappable)
+    cbar_ax.ax.set_ylim(distance.min(),distance.max())
+
+    # Show
+    plt.savefig(f"{config.PCA_ICA_FOLDER}/pca.png", dpi=300)
 
 # Apply FastICA
 # If the PCA analysis showed us that only a few components 
