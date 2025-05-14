bab42956dbcde3b82633441e083bdd727c79b7b6
hongmaohuang <al47777al4@gmail.com>
2025-05-14 19:02:21 +0800

    Cat all waterfall images as a long vector, making sure the consistency of clustering result.

--- Sliding_Window_with_1D_DSS/02_Feature_Functions.py
+++ Sliding_Window_with_1D_DSS/02_Feature_Functions.py
@@ -11,7 +11,9 @@ os.makedirs(config.FEATURES_FOLDER)
 
 # ==== Load All Waterfall Images ====
 file_list = sorted(glob.glob(f'{config.WATERFALL_NPZ_FOLDER}/*.npz'))
-
+feat_1_all = []
+feat_2_all = []
+feat_3_all = []
 for file in file_list:
     basename = os.path.splitext(os.path.basename(file))[0]
     dt_str = basename.split('_')[2] + basename.split('_')[3]
@@ -30,38 +32,27 @@ for file in file_list:
         print(f"Total Distance: {config.TOTAL_DISTANCE_KM:.2f} km\n")
 
     # ==== Extract Features across Distance ====
-    features_list = []
-    window_centers_distance = []
     print(f"Extracting features every {config.STEP_CHANNEL} pixels with a window size of {config.WINDOW_SIZE_CHANNEL} pixels\n")
     
     for j in range(0, num_channels - config.WINDOW_SIZE_CHANNEL + 1, config.STEP_CHANNEL):
         window = DAS_data[:, j:j+config.WINDOW_SIZE_CHANNEL]
-        mean_time_series = np.mean(window, axis=1)
-        #plt.plot(mean_time_series)
-        #plt.show()
-        #break
-        #fft_result = np.fft.fft(mean_time_series)
-        #fft_freq = np.fft.fftfreq(len(mean_time_series))  
-        #half = len(fft_result) // 2
-        #fft_result = fft_result[:half]
-        #fft_freq = fft_freq[:half]
-        #print(np.abs(fft_result))
-        #plt.figure(figsize=(10, 6))
-        #plt.plot(fft_freq, np.abs(fft_result))  
-        #plt.xlabel('Frequency')
-        #plt.ylabel('Amplitude')
-        #plt.title('FFT of Mean Per Row')
-        #plt.grid(True)
-        #plt.tight_layout()
-        #plt.show()
-        #feat_1 = np.percentile(np.abs(fft_result), 75) 
         feat_1 = np.median(window)
         feat_2 = window.std()
         feat_3 = np.percentile(window, 75)
-        feature_vector = np.array([feat_1, feat_2, feat_3])
-        features_list.append(feature_vector)
-        center_distance = (j + config.WINDOW_SIZE_CHANNEL / 2) * distance_per_channel
-        window_centers_distance.append(center_distance)
-    features_array = np.vstack(features_list)
-    output_path = os.path.join(config.FEATURES_FOLDER, f"features_{basename.split('_')[2]+'_'+basename.split('_')[3]}.npz")
-    np.savez(output_path, features=features_array)
+        feat_1_all.append(feat_1)
+        feat_2_all.append(feat_2)
+        feat_3_all.append(feat_3)
+
+feature_vector = np.array([feat_1_all, feat_2_all, feat_3_all])
+#center_distance = (j + config.WINDOW_SIZE_CHANNEL / 2) * distance_per_channel
+#window_centers_distance.append(center_distance)
+features_array = np.vstack(feature_vector)
+base_no_ext = os.path.splitext(os.path.basename(file_list[0]))[0]
+final_no_ext = os.path.splitext(os.path.basename(file_list[-1]))[0]
+base_parts = base_no_ext.split('_')
+base_parts_final = final_no_ext.split('_')
+output_path = os.path.join(
+    config.FEATURES_FOLDER,
+    f"features_{base_parts[2]}_{base_parts[3]}-{base_parts_final[2]}_{base_parts_final[3]}.npz"
+)
+np.savez(output_path, features=features_array)
