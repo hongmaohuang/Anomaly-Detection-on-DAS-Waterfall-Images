36d660ba47dbc5941e56618a1641368b3d02a13d
hongmaohuang <al47777al4@gmail.com>
2025-05-27 18:55:15 +0800

    Add three features: 1.RMS STD, 2.Aspect*Slope, 3.Freq. Max.

--- Sliding_Window_with_1D_DSS/02_Feature_Functions.py
+++ Sliding_Window_with_1D_DSS/02_Feature_Functions.py
@@ -4,7 +4,12 @@ import numpy as np
 from datetime import datetime
 import config 
 import shutil
-import matplotlib.pyplot as plt
+import matplotlib.cm as cm, matplotlib.pyplot as plt
+from scipy.spatial import cKDTree
+from skimage import exposure
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
+
 if os.path.exists(config.FEATURES_FOLDER):
     shutil.rmtree(config.FEATURES_FOLDER)
 os.makedirs(config.FEATURES_FOLDER)
@@ -21,11 +26,31 @@ for idx, file in enumerate(file_list):
     dt = datetime.strptime(dt_str, "%Y%m%d%H%M%S")
     #print(f"\nProcessing {dt.strftime('%Y-%m-%d %H:%M:%S')}\n")
     data = np.load(file)
-    img_gray = data["waterfall"].mean(axis=2)
-    DAS_data = img_gray
-    num_time_samples, num_channels = DAS_data.shape
+    #img_gray = data["waterfall"].mean(axis=2)
+    wf_rgba = data["waterfall"]
+    wf_rgb  = wf_rgba[..., :3].astype(np.uint8)  
+    jet = cm.get_cmap("jet", 256)(np.arange(256))[:, :3]
+    idx_img = cKDTree(jet).query(wf_rgb.reshape(-1,3), k=1)[1]\
+                        .reshape(wf_rgb.shape[:2]).astype(np.uint8)
+    dmin, dmax = -113.0, -35.0
+    wf_db = dmin + idx_img.astype(np.float32)*(dmax-dmin)/255.0
+    wf_db_u8 = exposure.rescale_intensity(
+            wf_db, in_range=(dmin, dmax), out_range='uint8'
+        ).astype(np.uint8)
+    rms_img = wf_db_u8
+    num_time_samples, num_channels = rms_img.shape
+    dt_sec = config.DURATION_WATERFALL * 60 / num_time_samples         
+    dx_m   = config.TOTAL_DISTANCE_KM * 1000 / num_channels
+    dI_dt, dI_dx = np.gradient(rms_img, dt_sec, dx_m)
+    eps = 1e-6
+    slope_img = dI_dt / (dI_dx + eps)
+    aspect_rad = np.arctan2(dI_dt, dI_dx)
+    aspect_deg_img = (np.degrees(aspect_rad) + 360) % 360
+    asp_slo_prod_img = aspect_deg_img * slope_img
+
     time_per_sample = config.TOTAL_DURATION_SEC / num_time_samples
     distance_per_channel = config.TOTAL_DISTANCE_KM / num_channels
+    
     if file == file_list[0]:
         print(f"Time per Sample: {time_per_sample:.3f} sec")
         print(f"Distance per Channel: {distance_per_channel*1000:.5f} m")
@@ -34,10 +59,16 @@ for idx, file in enumerate(file_list):
         print(f"Extracting features every {config.STEP_CHANNEL} pixels with a window size of {config.WINDOW_SIZE_CHANNEL} pixels\n")
     
     for j in range(0, num_channels - config.WINDOW_SIZE_CHANNEL + 1, config.STEP_CHANNEL):
-        window = DAS_data[:, j:j+config.WINDOW_SIZE_CHANNEL]
-        feat_1 = np.median(window)
-        feat_2 = window.std()
-        feat_3 = np.percentile(window, 75)
+        window_1 = rms_img[:, j:j+config.WINDOW_SIZE_CHANNEL]
+        window_2 = asp_slo_prod_img[:, j:j+config.WINDOW_SIZE_CHANNEL]
+        feat_1 = window_1.std()
+        feat_2 = window_2.mean()
+        window_3 = rms_img[:, j:j+config.WINDOW_SIZE_CHANNEL].astype(float).mean(axis=1)
+        window_3 -= window_3.mean()
+        fft_vals = np.fft.rfft(window_3)
+        amp_spec = np.abs(fft_vals) / window_3.size
+        freqs    = np.fft.rfftfreq(window_3.size, d=dt_sec)
+        feat_3   = freqs[np.argmax(amp_spec)]
         feat_1_all.append(feat_1)
         feat_2_all.append(feat_2)
         feat_3_all.append(feat_3)
